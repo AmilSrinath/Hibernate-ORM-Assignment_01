5027ad5ce871da1de297c9210ab1f79a5d7c79f2
Amil Srinath <amilsrinath5@gmail.com>
2023-08-01 17:26:32 +0530

    Create Option Method in Main Class

--- Part_01/src/main/java/org/example/Main.java
+++ Part_01/src/main/java/org/example/Main.java
@@ -1,7 +1,59 @@
 package org.example;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        options();
+    }
+
+    public static void options() throws IOException {
+        Scanner input = new Scanner(System.in);
+        f:while (true) {
+            for (int i = 0; i < 30; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+            System.out.println("|        1 Book Save         |");
+            System.out.println("|        2 Book Update       |");
+            System.out.println("|        3 Book Delete       |");
+            System.out.println("|        4 Book Search       |");
+            for (int i = 0; i < 30; i++) {
+                System.out.print("-");
+            }
+            System.out.println();
+            System.out.print("Select Option : ");
+            int option = input.nextInt();
+            switch (option) {
+                case 1:
+                    saveBook();
+                case 2:
+                    updateBook();
+                case 3:
+                    deleteBook();
+                case 4:
+                    searchBook();
+                default:
+                    System.out.println("Invalid!!, Try Again!!");
+                    continue f;
+            }
+        }
+    }
+
+    public static void saveBook(){
+
+    }
+
+    public static void updateBook(){
+
+    }
+
+    public static void deleteBook(){
+
+    }
+
+    public static void searchBook(){
+
     }
 }
