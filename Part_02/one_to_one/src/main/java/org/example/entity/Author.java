fb1dcd4dce34cada11cdf15b217df07635e58e92
Amil Srinath <amilsrinath5@gmail.com>
2023-08-01 21:16:32 +0530

    Update Author.java

--- Part_02/one_to_one/src/main/java/org/example/entity/Author.java
+++ Part_02/one_to_one/src/main/java/org/example/entity/Author.java
@@ -1,4 +1,45 @@
 package org.example.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Author {
+    @Id
+    private String aid;
+    private String name;
+    private int email;
+
+    public String getAid() {
+        return aid;
+    }
+
+    public void setAid(String aid) {
+        this.aid = aid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEmail() {
+        return email;
+    }
+
+    public void setEmail(int email) {
+        this.email = email;
+    }
+
+    public Author() {
+    }
+
+    public Author(String aid, String name, int email) {
+        this.aid = aid;
+        this.name = name;
+        this.email = email;
+    }
 }
