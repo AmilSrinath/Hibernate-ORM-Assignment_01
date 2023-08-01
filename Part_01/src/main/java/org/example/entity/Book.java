ccdd1cb553227fb785297cea99b25f1b79c8751c
Amil Srinath <amilsrinath5@gmail.com>
2023-08-01 17:19:09 +0530

    Update Book Entity

--- Part_01/src/main/java/org/example/entity/Book.java
+++ Part_01/src/main/java/org/example/entity/Book.java
@@ -1,5 +1,75 @@
 package org.example.entity;
 
-public class Book {
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
+@Entity
+public class Book {
+    @Id
+    private String bid;
+    private String ISBN;
+    private String title;
+    private String author;
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Book(String bid, String ISBN, String title, String author) {
+        this.bid = bid;
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book() {
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bid='" + bid + '\'' +
+                ", ISBN='" + ISBN + '\'' +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public Book(String ISBN, String title, String author) {
+        this.ISBN = ISBN;
+        this.title = title;
+        this.author = author;
+    }
+
+    public Book(String bid) {
+        this.bid = bid;
+    }
 }
